16d9f5f3c149e266144241a8679f62a15199aa17
Irfan <irfantayyib07@gmail.com>
2025-03-10 10:41:52 +0500

    Add author credit to header component

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -4,6 +4,7 @@ function Header({}) {
    <div className="min-h-16 bg-stone-300 w-full flex flex-col items-center px-6 py-3 uppercase font-semibold">
     Task Manager
     <small>Sprouto Group</small>
+    <small>By Irfan Tayyab</small>
    </div>
   </>
  );
